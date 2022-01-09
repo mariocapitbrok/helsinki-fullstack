f25b7db90eda4f79ea1f2b8f235c42c79a356c7d
Mario Capitbrok <mario@capitbrok.com>
2022-01-09 11:23:34 -0600

    history

---
+++ .history/part1/unicafe/src/App_20220109105803.js
@@ -0,0 +1,97 @@
+import React, { useState } from 'react'
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+const StatisticLine = ({ title, value }) => (
+  <li>
+    {title} {title === 'positive' ? value + ' %' : value}
+  </li>
+)
+
+const Statistics = ({ good, neutral, bad, total, average, percentage }) => {
+  if (total === 0)
+    return (
+      <>
+        <h1>statistics</h1>
+        <h3>No feedback given</h3>
+      </>
+    )
+
+  return (
+    <>
+      <h1>statistics</h1>
+      <div>
+        <StatisticLine title="good" value={good} />
+        <StatisticLine title="neutral" value={neutral} />
+        <StatisticLine title="bad" value={bad} />
+        <StatisticLine title="all" value={total} />
+        <StatisticLine title="average" value={average} />
+        <StatisticLine title="positive" value={percentage} />
+      </div>
+    </>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [percentage, setPercentage] = useState(0)
+
+  const handleGood = () => {
+    const newGood = good + 1
+    const newTotal = newGood + neutral + bad
+    const newAverage = (newGood - bad) / newTotal
+    const newPercentage = ((newGood / newTotal) * 100).toFixed(14)
+
+    setGood(newGood)
+    setTotal(newTotal)
+    setAverage(newAverage)
+    setPercentage(newPercentage)
+  }
+
+  const handleNeutral = () => {
+    const newNeutral = neutral + 1
+    const newTotal = good + newNeutral + bad
+    const newAverage = (good - bad) / newTotal
+    const newPercentage = ((good / newTotal) * 100).toFixed(14)
+
+    setNeutral(newNeutral)
+    setTotal(newTotal)
+    setAverage(newAverage)
+    setPercentage(newPercentage)
+  }
+
+  const handleBad = () => {
+    const newBad = bad + 1
+    const newTotal = good + neutral + newBad
+    const newAverage = (good - newBad) / newTotal
+    const newPercentage = ((good / newTotal) * 100).toFixed(14)
+
+    setBad(newBad)
+    setTotal(newTotal)
+    setAverage(newAverage)
+    setPercentage(newPercentage)
+  }
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button onClick={handleGood} text="good" />
+      <Button onClick={handleNeutral} text="neutral" />
+      <Button onClick={handleBad} text="bad" />
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={total}
+        average={average}
+        percentage={percentage}
+      />
+    </div>
+  )
+}
+
+export default App
